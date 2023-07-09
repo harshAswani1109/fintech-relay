44286f296a4ad002ff46029809d2559b73ef731e
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-07-10 03:39:18 +0530

    minor changes

--- components/landing/landing.js
+++ components/landing/landing.js
@@ -1,16 +1,17 @@
 import Link from "next/link";
 import { VscActivateBreakpoints } from "react-icons/vsc";
+
 const Landing = () => {
   return (
     <div className="bg-gray-100">
       <header className="bg-gray-900 text-white py-4">
-        <h1 className="text-2xl text-center">Fintech Relay</h1>
+        <h1 className="text-4xl text-center font-bold">Fintech Relay</h1>
       </header>
 
       <section className="py-10">
         <div className="flex items-center justify-center">
           <div className="max-w-md bg-white shadow-lg rounded-lg p-6">
-            <h2 className="text-2xl font-bold mb-4">
+            <h2 className="text-3xl font-bold mb-4">
               Meet Fintech Relay, your fintech advisor
             </h2>
             <p className="text-gray-700">
@@ -23,57 +24,57 @@ const Landing = () => {
 
       <section className="bg-gray-200 py-10">
         <div className="max-w-md mx-auto px-4">
-          <h3 className="text-2xl mb-4">Features</h3>
-          <ul className=" flex flex-col justify-center items-center">
-            <div className="flex items-center mb-6 hover:text-blue-500 transition-colors justify-center gap-10 w-full ">
-              <VscActivateBreakpoints className="h-24 w-24" />
+          <h3 className="text-3xl mb-4">Features</h3>
+          <ul className="grid grid-cols-1 gap-6">
+            <li className="flex items-center hover:text-blue-500 transition-colors">
+              <VscActivateBreakpoints className="w-16 h-16 text-gray-600 mr-4" />
               <span>
-                <span className="font-semibold">
+                <span className="font-semibold text-xl">
                   Transaction Fee Calculation:
                 </span>{" "}
-                After entering the transaction amount,the transaction fee for
-                each selected product based on the transaction fee percentage
-                provided.
+                After entering the transaction amount, the component calculates
+                the transaction fee for each selected product based on the
+                transaction fee percentage provided.
               </span>
-            </div>
-            <div className="flex items-center mb-6 hover:text-blue-500 transition-colors justify-center gap-10 w-full ">
-              <VscActivateBreakpoints className="h-24 w-24" />
+            </li>
+            <li className="flex items-center hover:text-blue-500 transition-colors">
+              <VscActivateBreakpoints className="w-16 h-16 text-gray-600 mr-4" />
               <span>
-                <span className="font-semibold">Display Results:</span> The
-                component displays the transaction fees for each selected
+                <span className="font-semibold text-xl">Display Results:</span>{" "}
+                The component displays the transaction fees for each selected
                 product, along with their features. It also highlights the
                 product with the lowest fee as the best option.
               </span>
-            </div>
+            </li>
           </ul>
         </div>
       </section>
 
       <section className="bg-gray-200 py-10">
         <div className="max-w-md mx-auto px-4">
-          <h3 className="text-2xl mb-4">Testimonials</h3>
-          <div className="grid grid-cols-1 gap-4">
-            <div className="bg-white rounded-lg shadow-lg px-6 py-2">
+          <h3 className="text-3xl mb-4">Testimonials</h3>
+          <div className="grid grid-cols-1 gap-6">
+            <div className="bg-white rounded-lg shadow-lg px-6 py-8">
               <div className="flex items-center">
-                <VscActivateBreakpoints className="w-40 h-40 rounded-full mr-10" />
+                <VscActivateBreakpoints className="w-20 h-20 text-gray-600 rounded-full mr-6" />
                 <div>
-                  <p className="text-gray-800 font-medium">
+                  <p className="text-gray-800 text-lg">
                     "Fintech Relay has been a game-changer in my daily life.
                     It's like having a personal assistant always at my side."
                   </p>
-                  <footer className="text-gray-600">- Ayush Garg</footer>
+                  <footer className="text-gray-600 mt-2">- Ayush Garg</footer>
                 </div>
               </div>
             </div>
-            <div className="bg-white rounded-lg shadow-lg px-6 py-2">
+            <div className="bg-white rounded-lg shadow-lg px-6 py-8">
               <div className="flex items-center">
-                <VscActivateBreakpoints className="w-40 h-40 rounded-full mr-10" />
+                <VscActivateBreakpoints className="w-20 h-20 text-gray-600 rounded-full mr-6" />
                 <div>
-                  <p className="text-gray-800 font-medium">
+                  <p className="text-gray-800 text-lg">
                     "I'm amazed by the capabilities of Fintech Relay. It has
                     simplified my tasks and made everything more convenient."
                   </p>
-                  <footer className="text-gray-600">- Babla</footer>
+                  <footer className="text-gray-600 mt-2">- Babla</footer>
                 </div>
               </div>
             </div>
@@ -82,11 +83,14 @@ const Landing = () => {
       </section>
 
       <section className="py-10">
-        <Link className="flex items-center justify-center" href="/compare">
-          <button className="px-6 py-3 bg-gray-900 text-white text-xl rounded-lg hover:bg-gray-700">
-            Get Started with Fintech Relay
-          </button>
-        </Link>
+        <div className="flex items-center justify-center">
+          <Link href="/compare" className="relative inline-block">
+            <button className="relative z-10 px-6 py-3 bg-gray-900 text-white text-xl rounded-lg hover:bg-gray-700 hover:shadow-lg transition-all duration-300">
+              Get Started with Fintech Relay
+            </button>
+            <div className="absolute inset-0 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-lg"></div>
+          </Link>
+        </div>
       </section>
     </div>
   );
